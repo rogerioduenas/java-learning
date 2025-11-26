8443dcbe21bc091dac9444cb3a1006322d2c09c9
rogerioduenas <rogerioduenasdev@hotmail.com>
2025-11-26 13:04:58 +0900

    lesson 45: loops

---
+++ src/main/java/lesson45loops/Loops.java
@@ -0,0 +1,32 @@
+package lesson45loops;
+
+public class Loops {
+
+  static void main() {
+    loopFor();
+    loopWhile();
+    loopDoWhile();
+  }
+
+  static void loopFor() {
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i);
+    }
+  }
+
+  static void loopWhile() {
+    int number = 1;
+    while (number <= 10) {
+      System.out.println(number);
+      number++;
+    }
+  }
+
+  static void loopDoWhile() {
+    int number = 1;
+    do {
+      System.out.println(number);
+      number++;
+    } while (number <= 10);
+  }
+}
