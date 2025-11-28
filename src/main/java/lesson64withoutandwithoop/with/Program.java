9d9c3294da01ae9f39f1147b4f52376133480f1a
rogerioduenas <rogerioduenasdev@hotmail.com>
2025-11-28 13:57:58 +0900

    lesson 64.2: problem solved with oop

---
+++ src/main/java/lesson64withoutandwithoop/with/Program.java
@@ -0,0 +1,38 @@
+package lesson64withoutandwithoop.with;
+
+import lesson64withoutandwithoop.with.entities.Triangle;
+
+import java.util.Scanner;
+
+public class Program {
+
+  static void main(){
+    Scanner sc = new Scanner(System.in);
+    Triangle x = new Triangle();
+    Triangle y = new Triangle();
+
+    System.out.println("Enter the measures of triangle X");
+    x.a = sc.nextDouble();
+    x.b = sc.nextDouble();
+    x.c = sc.nextDouble();
+
+    System.out.println("Enter the measures of triangle X");
+    y.a = sc.nextDouble();
+    y.b = sc.nextDouble();
+    y.c = sc.nextDouble();
+
+    double areaX = x.area();
+    double areaY = y.area();
+
+    System.out.printf("Area of triangle X is %.2f\n", areaX);
+    System.out.printf("Area of triangle Y is %.2f\n", areaY);
+
+    if (areaX > areaY) {
+      System.out.printf("Larger area is X: %.2f\n", areaX);
+    } else {
+      System.out.printf("Larger area is Y: %.2f\n", areaY);
+    }
+
+    sc.close();
+  }
+}
---
+++ src/main/java/lesson64withoutandwithoop/with/entities/Triangle.java
@@ -0,0 +1,12 @@
+package lesson64withoutandwithoop.with.entities;
+
+public class Triangle {
+
+  public double a, b, c;
+
+  public double area(){
+    double p = (a + b + c) / 2;
+    double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    return result;
+  }
+}
