94138d9f06f22ce67d46f912d34490676b3119eb
rogerioduenas <rogerioduenasdev@hotmail.com>
2025-12-10 16:26:24 +0900

    lesson 102: loop forEach

--- src/main/java/module06/lesson45loops/Loops.java
+++ src/main/java/module06/lesson45loops/Loops.java
@@ -6,15 +6,18 @@ public class Loops {
     loopFor();
     loopWhile();
     loopDoWhile();
+    loopForEach();
   }
 
   static void loopFor() {
+    System.out.println("---------- Loop for ----------");
     for (int i = 1; i <= 10; i++) {
       System.out.println(i);
     }
   }
 
   static void loopWhile() {
+    System.out.println("---------- Loop While ----------");
     int number = 1;
     while (number <= 10) {
       System.out.println(number);
@@ -23,10 +26,19 @@ public class Loops {
   }
 
   static void loopDoWhile() {
+    System.out.println("---------- Loop do While ----------");
     int number = 1;
     do {
       System.out.println(number);
       number++;
     } while (number <= 10);
+  }
+
+  static void loopForEach() {
+    System.out.println("----------Loop forEach ----------");
+    String[] vect = new String[]{"Mike", "Anna", "Caio"};
+    for (String value : vect) {
+      System.out.println(value);
+    }
   }
 }
