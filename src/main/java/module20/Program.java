bf964c35de789620731409e8d4b451a01b67e435
rogerioduenas <rogerioduenasdev@hotmail.com>
2026-03-16 17:14:24 +0900

    lesson 246: data insertion

--- src/main/java/module20/Program.java
+++ src/main/java/module20/Program.java
@@ -6,10 +6,19 @@ public class Program {
   public static void main(String[] args) throws SQLException {
 
     System.out.println("========== READ DATA ==========");
+    System.out.println();
     System.out.println("--- departments ---");
     Read.readDepartments();
+    System.out.println();
     System.out.println("--- sellers ---");
     Read.readSellers();
 
+    System.out.println("========== INSERT DATA ==========");
+    System.out.println();
+    System.out.println("--- sellers ---");
+    Insert.insertSeller("mike");
+    System.out.println();
+    System.out.println("--- departments ---");
+    Insert.insertMultipleDepartments();
   }
 }
